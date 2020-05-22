d607f9047dd7e27d9f81d25bdb5a269fcd1d9555
Ayush <ayush@technoscore.net>
2020-05-22 09:58:24 +0530

    1.0.0

--- SupplyApply/Helpers/DBManager.swift
+++ SupplyApply/Helpers/DBManager.swift
@@ -13,58 +13,58 @@ let notificationTableName = "NotificationTable"
 
 class DBManager: NSObject {
 
-    static func getAppDelegate() -> AppDelegate? {
-        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-            return appDelegate
-        } else {
-            return nil
-        }
-    }
-
-    static func getManagedContext()-> NSManagedObjectContext? {
-        let context = getAppDelegate()?.persistentContainer.viewContext
-        return context
-    }
-
-    static func saveContext() {
-        getAppDelegate()?.saveContext()
-    }
+//    static func getAppDelegate() -> AppDelegate? {
+//        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+//            return appDelegate
+//        } else {
+//            return nil
+//        }
+//    }
+//
+//    static func getManagedContext()-> NSManagedObjectContext? {
+//        let context = getAppDelegate()?.persistentContainer.viewContext
+//        return context
+//    }
+//
+//    static func saveContext() {
+//        getAppDelegate()?.saveContext()
+//    }
+//    
+//    static func addNotificationToDB(name : String?, time:String) {
+//        
+//        let managedContext = DBManager.getManagedContext()
+//        let request = NSFetchRequest<NSManagedObject>(entityName: notificationTableName)
+//        request.returnsObjectsAsFaults = false
+//        let data = NotificationTable(context: managedContext!)
+//        data.name = name
+//        data.time = time
+//        DBManager.saveContext()
+//        print(request)
+//    }
     
-    static func addNotificationToDB(name : String?, time:String) {
-        
-        let managedContext = DBManager.getManagedContext()
-        let request = NSFetchRequest<NSManagedObject>(entityName: notificationTableName)
-        request.returnsObjectsAsFaults = false
-        let data = NotificationTable(context: managedContext!)
-        data.name = name
-        data.time = time
-        DBManager.saveContext()
-        print(request)
-    }
-    
-    static func fetchNotificationDetailToDB() -> ([String] , [String]) {
-        
-        var notificationName = [String]()
-        var notificationTime = [String]()
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        let request = NSFetchRequest<NSManagedObject>(entityName: notificationTableName)
-        request.returnsObjectsAsFaults = false
-        
-        do {
-            
-            let results = try context.fetch(request) as? [NotificationTable]
-            if results?.count != 0 {
-                for i in 0..<(results?.count)! {
-                    notificationName.append(results?[i].name ?? "")
-                    notificationTime.append(results?[i].time ?? "")
-                }
-            }
-            print(request)
-        } catch {
-            print("Failed")
-        }
-        
-        return (notificationName, notificationTime)
-    }
+//    static func fetchNotificationDetailToDB() -> ([String] , [String]) {
+//        
+//        var notificationName = [String]()
+//        var notificationTime = [String]()
+//        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//        let context = appDelegate.persistentContainer.viewContext
+//        let request = NSFetchRequest<NSManagedObject>(entityName: notificationTableName)
+//        request.returnsObjectsAsFaults = false
+//        
+//        do {
+//            
+//            let results = try context.fetch(request) as? [NotificationTable]
+//            if results?.count != 0 {
+//                for i in 0..<(results?.count)! {
+//                    notificationName.append(results?[i].name ?? "")
+//                    notificationTime.append(results?[i].time ?? "")
+//                }
+//            }
+//            print(request)
+//        } catch {
+//            print("Failed")
+//        }
+//        
+//        return (notificationName, notificationTime)
+//    }
 }
